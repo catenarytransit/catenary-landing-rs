c3980c07330843d416737fac78253205628f06b1
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-03-03 02:42:07 -0800

    add i18n and leptos dark

---
+++ src/app.rs
@@ -0,0 +1,221 @@
+use crate::i18n::*;
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+
+    view! {
+            // injects a stylesheet into the document <head>
+            // id=leptos means cargo-leptos will hot-reload this stylesheet
+            <Stylesheet id="leptos" href="/pkg/kylerchinmusic.css"/>
+            <Stylesheet href="https://use.typekit.net/nhx0pgc.css"/>
+
+            // content for this welcome page
+            <Router>
+                <main class="">
+                    <Routes>
+                        <Route path="" view=HomePage
+                        ssr=SsrMode::Async
+                        />
+                        <Route path="/*any" view=NotFound/>
+                    </Routes>
+                </main>
+            </Router>
+        }
+}
+
+#[component]
+fn Nav() -> impl IntoView {
+    leptos_meta::provide_meta_context();
+
+    provide_i18n_context();
+
+    let i18n = use_i18n();
+
+    view! {
+        <div class="flex flex-row">
+            <div>"Catenary Maps"</div>
+        </div>
+    }
+}
+
+#[component]
+fn Footer() -> impl IntoView {
+    leptos_meta::provide_meta_context();
+    provide_i18n_context();
+    let i18n = use_i18n();
+
+    view! {
+    <div class="w-full catenary-sea text-white px-8 py-4 text-sm  mt-auto">
+
+    </div>
+    }
+}
+
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    leptos_meta::provide_meta_context();
+
+    provide_i18n_context();
+
+    let i18n = use_i18n();
+
+    let darkmode = Darkmode::init();
+
+    view! {
+
+        <Title text={t!(i18n, title)}/>
+
+        <Html lang="en" class=move || if darkmode.is_dark() { "dark" } else { "" }/>
+        // sets the document title
+        <div
+        class="catenary-sea flex flex-col h-full"
+        >
+
+        <div class="w-full">
+        <div class="catenary-sea">
+        <Navbar
+        white_text={true}
+        />
+
+        <div>
+        
+        <div>
+        <h1><span>"Catenary helps "</span><span>"connect"</span><span>" people and communities."</span></h1>
+        </div>
+
+        </div>
+
+        </div>
+       
+        </div>
+        <Footer/>
+        </div>
+
+    }
+}
+
+fn invert_bool(n: &mut bool) {
+    if *n == false {
+        *n = true;
+    } else {
+        *n = false;
+    }
+}
+
+#[component]
+fn PickTranslation(set_opened: WriteSignal<bool>) -> impl IntoView {
+    leptos_meta::provide_meta_context();
+
+    provide_i18n_context();
+
+    let i18n = use_i18n();
+
+    leptos_i18n::load_locales!();
+
+    let avaliable_lang = vec![(Locale::en, "English"), (Locale::zh, "简体中文"), (Locale::zh_TW, "繁體中文"), (Locale::ko, "한국어")];
+
+    view! {
+        {
+            avaliable_lang.into_iter().map(move |lang_pair| {
+                view! {
+
+                    <div on:click=move |_| {i18n.set_locale(lang_pair.0); set_opened.update(|n| *n = false);}>
+                        {lang_pair.1}
+                    </div>
+                }
+            })
+            .collect_view()
+        }
+    }
+}
+
+#[component]
+fn Navbar(white_text: bool) -> impl IntoView {
+
+    leptos_meta::provide_meta_context();
+
+    provide_i18n_context();
+
+    let i18n = use_i18n();
+
+    let (opened, set_opened) = create_signal(false);
+
+    view! {
+        <header
+        class="fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out bg-background backdrop-blur-sm shadow-lg text-white"
+    >
+        <div class="max-w-6xl mx-auto px-5 sm:px-6">
+            <div class="flex items-center justify-between h-16 md:h-20">
+                <div class="shrink-0 mr-4">
+                    <a href="/">
+                        <span class="text-[1.3em]">
+                            <img
+                                src="/assets/images/favicon.png"
+                                class="logoLight inline-block mr-4"
+                                alt={"Catenary Logo"}
+                                width={50}
+                                height={30}
+                            />
+                            "Catenary"
+                        </span>
+                    </a>
+                </div>
+                {/* Desktop navigation */}
+                <nav class="flex grow">
+                    {/* Desktop sign in links */}
+                    <ul class="flex grow justify-end flex-wrap items-center">
+                        <li class="md:order-2">
+                            <a
+                                href="https://maps.catenarymaps.org"
+                                class="btn-sm text-foreground bg-secondary hover:bg-tertiary ml-4"
+                            >
+                                <span>{t!(i18n,launchmaps)}</span>
+                                <svg
+                                    class="w-3 h-3 fill-current text-muted-foreground shrink-0 ml-2 -mr-1"
+                                    viewBox="0 0 12 12"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                                        fillRule="nonzero"
+                                    />
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="hidden md:flex mb-4 md:ml-4 md:mb-0">
+                           
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+        }
+}
+
+/// 404 - Not Found
+#[component]
+fn NotFound() -> impl IntoView {
+    // set an HTTP status code 404
+    // this is feature gated because it can only be done during
+    // initial server-side rendering
+    // if you navigate to the 404 page subsequently, the status
+    // code will not be set because there is not a new HTTP request
+    // to the server
+    #[cfg(feature = "ssr")]
+    {
+        // this can be done inline because it's synchronous
+        // if it were async, we'd use a server function
+        let resp = expect_context::<leptos_actix::ResponseOptions>();
+        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
+    }
+
+    view! {
+        <h1>"Not Found"</h1>
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,137 @@
+#[cfg(feature = "ssr")]
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_files::Files;
+    use actix_web::dev::Service;
+    use actix_web::http::Uri;
+    use actix_web::HttpResponse;
+    use actix_web::Responder;
+    use actix_web::*;
+    use catenarylanding::app::*;
+    use leptos::*;
+    use leptos_actix::{generate_route_list, LeptosRoutes};
+    use qstring::QString;
+
+    use core::future::Future;
+    use futures::future::ok;
+    use futures::future::Either;
+    use futures::future::Ready;
+    use futures::FutureExt;
+
+    let conf = get_configuration(None).await.unwrap();
+    let addr = conf.leptos_options.site_addr;
+    // Generate the list of routes in your Leptos App
+    let routes = generate_route_list(App);
+    println!("listening on http://{}", &addr);
+
+    HttpServer::new(move || {
+        let leptos_options = &conf.leptos_options;
+        let site_root = &leptos_options.site_root;
+
+        App::new()
+            // serve JS/WASM/CSS from `pkg`
+            .service(Files::new("/pkg", format!("{site_root}/pkg")))
+            // serve other assets from the `assets` directory
+            .service(Files::new("/assets", site_root))
+            // serve the favicon from /favicon.ico
+            .service(favicon)
+            .service(ads)
+            .wrap_fn(|service_request, service| {
+                let uri_original = service_request.uri().clone();
+
+                let mut uri_new = Uri::builder().scheme("https").authority("catenarymaps.org");
+
+                if let Some(p_and_q) = uri_original.path_and_query() {
+                    uri_new = uri_new.path_and_query(p_and_q.as_str());
+                }
+
+                let uri = format!("{}", uri_new.build().unwrap());
+
+                service.call(service_request).map(move |result| {
+                    let languages = vec!["en", "ko", "zh", "zh-TW"];
+
+                    let language_final_header = languages
+                        .iter()
+                        .map(|lang_code| {
+                            format!(
+                                "<{}>; rel=\"alternate\"; hreflang=\"{}\"",
+                                uri,
+                                lang_code
+                            )
+                        })
+                        .collect::<Vec<String>>()
+                        .join(",");
+
+                    let mut service_response = result.unwrap();
+
+                    let header_value = http::header::HeaderValue::from_str(&language_final_header);
+
+                    match header_value {
+                        Ok(header_value) => {
+                            service_response.headers_mut().insert(
+                                http::header::HeaderName::from_static("link"),
+                                header_value,
+                            );
+                        }
+                        Err(err) => {
+                            println!("{:#?}", err);
+                        }
+                    }
+
+                    Ok(service_response)
+                })
+            })
+            .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
+            .app_data(web::Data::new(leptos_options.to_owned()))
+            .wrap(middleware::Compress::default())
+    })
+    .bind(&addr)?
+    .run()
+    .await
+}
+
+#[cfg(feature = "ssr")]
+#[actix_web::get("favicon.ico")]
+async fn favicon(
+    leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
+) -> actix_web::Result<actix_files::NamedFile> {
+    let leptos_options = leptos_options.into_inner();
+    let site_root = &leptos_options.site_root;
+    Ok(actix_files::NamedFile::open(format!(
+        "{site_root}/favicon.ico"
+    ))?)
+}
+
+#[cfg(feature = "ssr")]
+#[actix_web::get("ads.txt")]
+async fn ads(
+    leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
+) -> actix_web::Result<actix_files::NamedFile> {
+    let leptos_options = leptos_options.into_inner();
+    let site_root = &leptos_options.site_root;
+    Ok(actix_files::NamedFile::open(format!(
+        "{site_root}/ads.txt"
+    ))?)
+}
+
+#[cfg(not(any(feature = "ssr", feature = "csr")))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for pure client-side testing
+    // see lib.rs for hydration function instead
+    // see optional feature `csr` instead
+}
+
+#[cfg(all(not(feature = "ssr"), feature = "csr"))]
+pub fn main() {
+    // a client-side main function is required for using `trunk serve`
+    // prefer using `cargo leptos serve` instead
+    // to run: `trunk serve --open --features csr`
+    use catenarylanding::app::*;
+    use leptos::*;
+    use wasm_bindgen::prelude::wasm_bindgen;
+
+    console_error_panic_hook::set_once();
+
+    leptos::mount_to_body(App);
+}
